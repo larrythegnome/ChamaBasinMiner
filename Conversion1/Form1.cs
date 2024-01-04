6e0d5c6adeecf01e54e01659db568515d7b34902
Logan <73564929+larrythegnome@users.noreply.github.com>
2024-01-04 13:07:07 -0500

    Add project files.

---
+++ Conversion1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Button = System.Windows.Forms.Button;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace ChamaBasinMiner
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        
+        private void makeLabel(int x, int y, String str)
+        {
+          
+        }
+
+        
+        private void userInputInt(TextBox t)
+        {
+           
+        }
+
+        
+        private void Next_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    
+        private void makeMineralsProcess(String str)
+        {
+           
+        }
+        
+        private void userInputMineralSymbol()
+        {
+            
+        }
+        
+        private void userInputTempMineralName()
+        {
+            
+        }
+         
+        private void userInputTempTonnage()
+        {
+            
+
+        }
+
+        
+        private void Enter_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+        
+    }
+}
---
+++ Conversion1/Minerals.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing.Text;
+
+namespace ChamaBasinMiner
+{
+    internal class Minerals
+    {
+        String symbol;
+        String name;
+        double marketRate;
+        int tonnage;
+        int min = 1;
+        int max = 200;
+        public Minerals()
+        {
+            symbol = "";
+            name = "";
+            marketRate = setMarketRate();
+            tonnage = 0;
+        }
+
+        public Minerals(String symbol, String name, int tonnage)
+        {
+            this.symbol = symbol;
+            this.name = name;
+            this.tonnage = tonnage;
+            marketRate = setMarketRate();
+        }
+
+        
+        private double setMarketRate()
+        {
+            
+            return 1;
+        }
+        
+        public double getValue()  
+        {
+            
+            return 1;
+        }
+        
+        public String toString()  
+        {
+            
+            return "";
+        }
+    }
+}
